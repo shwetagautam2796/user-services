60e474316146dc2fed1502cefb9e360aa0c74240
swati gautam <swatigautam2796@gmail.com>
2025-03-25 19:27:09 +0530

    Skip authorization for login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  skip_before_action :authorize_request, only: [:create] # Skip authorization for login
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -24,4 +26,15 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user&.valid_password?(params[:password])
+      token = JsonWebToken.encode({ user_id: user.id })  # Encode user ID in token
+      render json: { token: token, user: { id: user.id, email: user.email} }, status: :ok
+    else
+      render json: { error: "Invalid email or password" }, status: :unauthorized
+    end
+  end
 end
