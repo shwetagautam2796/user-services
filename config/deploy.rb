6ef46ef6e65539cbbb5ff625a7cd485de125eb91
swati gautam <shwetagautam2796@gmail.com>
2025-04-03 14:01:22 +0530

    remove assets setting from deploye

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,6 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
-before "deploy:assets:precompile"
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
